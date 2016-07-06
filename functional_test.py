b766390eb7b1dc9613b63bd28f2ca50f05dafcca
stream <mystream@vip.qq.com>
2016-07-06 15:37:27 +0800

    using unittest module

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
+import unittest
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+    def tearDown(self):
+        browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
