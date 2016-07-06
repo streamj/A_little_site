d6d219255d441d6131c618f63b69140324ea1705
stream <mystream@vip.qq.com>
2016-07-06 23:17:58 +0800

    add use post

--- functional_test.py
+++ functional_test.py
@@ -22,14 +22,13 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
 
-        inputbox.send_keys('Keys.ENTER')
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
-
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
         self.fail('Finish the test')
 
     def tearDown(self):
