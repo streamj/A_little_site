9e8982eb219b0801cdfc5a0b5e62bdc5f3596e48
stream <mystream@vip.qq.com>
2016-07-10 17:02:54 +0800

    use get_absolute_url

--- lists/views.py
+++ lists/views.py
@@ -16,7 +16,8 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect('view_list',list_.id)
+            # redirect will call List get_absolute_url()
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
     return render(request, 'list.html', {'list': list_, 'error': error})
@@ -31,4 +32,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('view_list',list_.id)
+    return redirect(list_)
