16db5f116244d2ac770e441b91eb2ac72f0f1c0e
stream <mystream@vip.qq.com>
2016-07-10 16:43:06 +0800

    Refactor list view

--- lists/views.py
+++ lists/views.py
@@ -9,11 +9,14 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -22,8 +25,3 @@ def new_list(request):
         error = "You can't have an emtpy list item"
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id))
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
