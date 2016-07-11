6752940ddd415be079c4b6cace5d3f566d956400
stream <mystream@vip.qq.com>
2016-07-12 01:44:59 +0800

    rewrite login

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -5,9 +5,10 @@ import time
 
 
 class LoginTest(FunctionalTest):
+
     def test_login_with_persona(self):
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         self.switch_to_new_window('Mozilla Persona')
 
@@ -16,7 +17,7 @@ class LoginTest(FunctionalTest):
 
         self.switch_to_new_window('To-Do')
 
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
@@ -32,6 +33,7 @@ class LoginTest(FunctionalTest):
         self.fail('could not find window')
 
     def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=300).until(
+        # probably need set a bigger timeout
+        WebDriverWait(self.browser, timeout=30).until(
             lambda b: b.find_element_by_id(element_id)
         )
