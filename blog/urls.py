407aa4a6d9a63873a24799229e1afb63fb5e7354
stream <mystream@vip.qq.com>
2016-07-20 00:39:28 +0800

    add dynamic url

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include , url
 urlpatterns = patterns('',
     url(r'^home$', 'blog.views.blog_home', name='blog_home'),
     # match \d+ to id
-    url(r'^(?P<args>\d+)/$', 'blog.views.detail', name='detail'),
+    url(r'^(?P<id>\d+)/$', 'blog.views.detail', name='detail'),
 )
