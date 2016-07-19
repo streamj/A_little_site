1ddd8c05fe1c896019f043feed9926551c88dbf7
stream <mystream@vip.qq.com>
2016-07-19 23:48:55 +0800

    all works, so save it

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include , url
 
 urlpatterns = patterns('',
     url(r'^home$', 'blog.views.blog_home', name='blog_home'),
-    # match \d+ to args
-   # url(r'^(?P<args>\d+)/$', 'blog.views.detail', name='detail'),
+    # match \d+ to id
+    url(r'^(?P<args>\d+)/$', 'blog.views.detail', name='detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,12 @@ def home_page(request):
 
 
 def blog_home(request):
-    print(request)
     post_list = Article.objects.all()
     return render(request, 'blog/blog_home.html', {'post_list': post_list})
 
-#def detail(request, id):
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'blog/post.html', {'post': post})
