b9431d296829a75744b6b92e1efb6cae34bdc452
stream <mystream@vip.qq.com>
2016-07-20 20:40:59 +0800

    add pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@ from blog.models import Post
 
 urlpatterns = patterns('',
     #url(r'^$', 'blog.views.blog_home', name='blog_home'),
-    url(r'^$', ListView.as_view(model=Post)),
+    url(r'^(?P<page>\d+)?/?$', ListView.as_view(
+        model=Post,
+        paginate_by=5,
+    )),
     # match \d+ to id
     # url(r'^(?P<id>\d+)/$', 'blog.views.detail', name='detail'),
 )
