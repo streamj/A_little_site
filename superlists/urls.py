6f5beb0221bdff76f94fa0683f57c9c9a7af34ff
stream <mystream@vip.qq.com>
2016-07-20 22:05:34 +0800

    implement flat page and test passing

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', 'blog.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
     url(r'^blog/', include('blog.urls')),
+    # flat pages
+    url(r'^blog/', include('django.contrib.flatpages.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
